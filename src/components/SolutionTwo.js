9502068a2b05c7ad3e683baf4083ba0ba6d6910b
lebogang B Olifant <lebogangodeveloper@gmail.com>
2021-01-10 13:11:26 +0200

    add header for solution component

--- src/components/SolutionTwo.js
+++ src/components/SolutionTwo.js
@@ -4,12 +4,24 @@ import { DistanceData } from '../Data/DistanceData';
 export const SolutionTwo = () => {
   return (
     <>
+      <SolutionHeader />
       <div className='solution-container'>
         {DistanceData.map((data, key) => {
-          return <div key={key}>{data.ipv4 + ' , ' + data.geo}</div>;
+          return <div key={key}>{data.latitude + ' , ' + data.longitude}</div>;
         })}
       </div>
     </>
+  );
+};
+
+const SolutionHeader = () => {
+  return (
+    <div className='header'>
+      <p>
+        a function that takes in Latitude and Longitude.Returns sorted list of
+        the 10 rows from data.json
+      </p>
+    </div>
   );
 };
 
