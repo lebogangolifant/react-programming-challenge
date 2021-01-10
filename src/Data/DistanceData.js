f9a354931ecc080379092b14fc11b5598acb8ab5
lebogang B Olifant <lebogangodeveloper@gmail.com>
2021-01-10 12:35:06 +0200

    save json data  in a const file

---
+++ src/Data/DistanceData.js
@@ -0,0 +1,43 @@
+export const DistanceData = [
+  {
+    latitude: '30.45229',
+    longitude: '-91.11560',
+  },
+  {
+    latitude: '31.7333',
+    longitude: '-106.4833',
+  },
+  {
+    latitude: '32.791539',
+    longitude: '-96.815154',
+  },
+  {
+    latitude: '33.733902',
+    longitude: '-84.425719',
+  },
+  {
+    latitude: '34.0611957',
+    longitude: '-118.2916508',
+  },
+  {
+    latitude: '36.142092',
+    longitude: '-5.358946',
+  },
+  {
+    latitude: '37.8752777778',
+    longitude: '-122.253055556',
+  },
+  {
+    latitude: '38.944444',
+    longitude: '-77.455833',
+  },
+
+  {
+    latitude: '40.7778197',
+    longitude: '-74.077003',
+  },
+  {
+    latitude: '45.314',
+    longitude: '-73.876',
+  },
+];
